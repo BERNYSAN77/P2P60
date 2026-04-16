e4d6afd09ab17b0867c3227acd7178d6809bb655
laboratorio <laboratorio@udla.edu.ec>
2026-04-16 09:09:35 -0500

    lecturas terminadas ejercicio empleado

--- PrimerProgreso/src/ejerproducto/interfaz/MainES.java
+++ PrimerProgreso/src/ejerproducto/interfaz/MainES.java
@@ -1,5 +1,6 @@
 package ejerproducto.interfaz;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class MainES {
@@ -8,17 +9,21 @@ public class MainES {
         double sueldo;
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Ingrese el nombre: ");
-        nombre = sc.nextLine();
+       // System.out.print("Ingrese el nombre: ");
+        //nombre = sc.nextLine();
+        nombre = JOptionPane.showInputDialog("Ingrese el nombre: ");
         System.out.print("Ingrese el apellido: ");
         apellido = sc.nextLine();
         System.out.print("Ingrese el salario: ");
-        sueldo = sc.nextDouble();
-        System.out.println("Ingrese su ciudad de residencia: ");
+        //sueldo = sc.nextDouble();
+        sueldo = Double.parseDouble(sc.nextLine());
+        System.out.print("Ingrese su ciudad de residencia: ");
         ciudadResidencia = sc.nextLine();
-        System.out.println("Su nombre es: "+nombre+" "+apellido);
+       /* System.out.println("Su nombre es: "+nombre+" "+apellido);
         System.out.println("Sueldo actual: "+sueldo);
-        System.out.println("Ciudad de residencia: "+ciudadResidencia);
+        System.out.println("Ciudad de residencia: "+ciudadResidencia);*/
+        JOptionPane.showMessageDialog(null,"Su nombre es: "+nombre+" "+apellido+"\nSueldo actual: "
+                +sueldo+"\nCiudad de residencia: "+ciudadResidencia);
 
 
     }
