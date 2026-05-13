23fc92d7b692808df1b9da91b8370a8dbd234b38
laboratorio <laboratorio@udla.edu.ec>
2026-05-13 10:10:19 -0500

    listas

--- PrimerProgreso/src/examen/interfaz/MainLista.java
+++ PrimerProgreso/src/examen/interfaz/MainLista.java
@@ -2,39 +2,46 @@ package examen.interfaz;
 
 import examen.negocio.ModeloRiesgo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class MainModelo {
+public class MainLista {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        ModeloRiesgo mr = null;
         String nombre;
         double entrada;
         double confianza;
         int opc;
+        List<ModeloRiesgo> riesgos = new ArrayList<ModeloRiesgo>();
+        int indice = 0;
         do{
             menu();
             opc = Integer.parseInt(sc.nextLine());
             switch(opc){
-                case 1:{
-                    System.out.print("Ingrese el nombre del modelo: ");
-                    nombre = sc.nextLine();
-                    System.out.print("Ingrese el factor de confianza");
-                    confianza = Double.parseDouble(sc.nextLine());
-                    mr = new ModeloRiesgo(nombre,confianza);
-                    //arreglo[i] = new ModeloRiesgo(nombre,confianza);
-                    //lista.add(new ModeloRiesgo(nombre,confianza));
-
-                }break;
-                case 2:{
-                    if(mr != null){
-                        System.out.println("Ingrese el nuevo factor de confianza");
+                case 1:{System.out.print("Ingrese el nombre del modelo: ");
+                        nombre = sc.nextLine();
+                        System.out.print("Ingrese el factor de confianza");
                         confianza = Double.parseDouble(sc.nextLine());
-                        mr.setFactorConfianza(confianza);
-                      //  arreglo[i].setFactorConfianza(confianza);
-                      //  lista.get(i).setFactorConfianza(confianza);
-
+                        riesgos.add(new ModeloRiesgo(nombre,confianza));
+                    }break;
+                case 2:{
+                    if(!riesgos.isEmpty() ){
+                        System.out.print("Ingrese el nombre del modelo al que desea modificar el factor de confianza: ");
+                        nombre = sc.nextLine();
+                        boolean encontro = false;
+                        for(int i= 0; i < riesgos.size();i++){
+                            if(riesgos.get(i).getNombreModelo().equals(nombre)){
+                                System.out.println("Ingrese el nuevo factor de confianza");
+                                confianza = Double.parseDouble(sc.nextLine());
+                                riesgos.get(i).setFactorConfianza(confianza);
+                                encontro = true;
+                                break;
+                            }
+                        }
+                        if(!encontro)
+                            System.out.println("No existe el modelo");
                     } else
                         System.out.println("no existen modelos creados");
                 }break;
@@ -48,10 +55,11 @@ public class MainModelo {
 
                 }break;
                 case 4:{
-                    if(mr != null)
-                        System.out.println(mr);
-                    else
-                        System.out.println("no existen modelos creados");
+                    if(indice >= 1){
+                        for(int i = 0 ; i < indice; i++)
+                            System.out.println(riesgos[i]);
+                    } else
+                        System.out.println("No hay informacion al mostrar");
                 }break;
                 case 5:{
                     System.out.println("Gracias por usar nuestros modelos =)");
@@ -73,4 +81,8 @@ public class MainModelo {
         System.out.print("Ingrese una opción: ");
 
     }
+}
+
+
+
 }
