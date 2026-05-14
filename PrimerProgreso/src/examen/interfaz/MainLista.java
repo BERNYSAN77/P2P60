82f55e59029fe272b679f7dd7626d8ff5ca10d35
laboratorio <laboratorio@udla.edu.ec>
2026-05-14 08:20:27 -0500

    Listas terminado

--- PrimerProgreso/src/examen/interfaz/MainLista.java
+++ PrimerProgreso/src/examen/interfaz/MainLista.java
@@ -3,6 +3,7 @@ package examen.interfaz;
 import examen.negocio.ModeloRiesgo;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -46,29 +47,63 @@ public class MainLista {
                         System.out.println("no existen modelos creados");
                 }break;
                 case 3:{
-                    if(mr != null){
-                        System.out.println("Ingrese el valor de entrada al modelo");
-                        entrada = Double.parseDouble(sc.nextLine());
-                        System.out.println(mr.evaluar(entrada));
-                    }else
-                        System.out.println("no existen modelos creados");
+                    if(!riesgos.isEmpty() ){
+                        System.out.print("Ingrese el nombre del modelo al que desea modificar el factor de confianza: ");
+                        nombre = sc.nextLine();
+                        boolean encontro = false;
+                        for(ModeloRiesgo mr: riesgos)
+                            if(mr.getNombreModelo().equals(nombre)){
+                                System.out.println("Ingrese el valor de entrada al modelo");
+                                entrada = Double.parseDouble(sc.nextLine());
+                                System.out.println(mr.evaluar(entrada));
+                                encontro = true;
+                                break;
+                            }
 
+                        if(!encontro)
+                            System.out.println("No existe el modelo");
+                    } else
+                        System.out.println("no existen modelos creados");
                 }break;
                 case 4:{
-                    if(indice >= 1){
-                        for(int i = 0 ; i < indice; i++)
-                            System.out.println(riesgos[i]);
+                    if(!riesgos.isEmpty() ){
+                        Iterator <ModeloRiesgo> it = riesgos.iterator();
+                        System.out.print("Ingrese el nombre del modelo: ");
+                        nombre = sc.nextLine();
+                        boolean encontro = false;
+                        while (it.hasNext() && !encontro) {
+                            ModeloRiesgo mr = it.next();
+                            if(mr.getNombreModelo().equalsIgnoreCase(nombre)){
+                                System.out.println("Ingrese el valor de entrada al modelo");
+                                entrada = Double.parseDouble(sc.nextLine());
+                                System.out.println("Modelo: "+mr.getNombreModelo());
+                                System.out.println("Factor de confianza: "+mr.getFactorConfianza());
+                                System.out.println("Evaluación: " + mr.evaluar(entrada));
+                                encontro = true;
+                            }else
+                                System.out.println("No se encontró el modelo");
+                            }
                     } else
                         System.out.println("No hay informacion al mostrar");
                 }break;
                 case 5:{
+                    if(!riesgos.isEmpty() ){
+                        Iterator <ModeloRiesgo> it = riesgos.iterator();
+                        while (it.hasNext()){
+                            System.out.println(it.next());
+                        }
+                    } else
+                        System.out.println("No hay informacion al mostrar");
+                }break;
+
+                case 6:{
                     System.out.println("Gracias por usar nuestros modelos =)");
                 }break;
                 default:
                     System.out.println("No es una opción válida");
             }
 
-        }while(opc != 5);
+        }while(opc != 6);
 
     }
     public static void menu(){
@@ -76,9 +111,11 @@ public class MainLista {
         System.out.println("1. Crear Modelo");
         System.out.println("2. Modificar factor de confianza");
         System.out.println("3. Evaluar");
-        System.out.println("4. Mostrar datos");
-        System.out.println("5. Salir");
+        System.out.println("4. Simulación");
+        System.out.println("5. Mostrar todos los modelos");
+        System.out.println("6. Salir");
         System.out.print("Ingrese una opción: ");
+
 
     }
 }
