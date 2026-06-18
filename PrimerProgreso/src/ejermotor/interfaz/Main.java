3f062de0a695b388847acca8cf0f853a319eaccc
laboratorio <laboratorio@udla.edu.ec>
2026-06-18 09:06:06 -0500

    excepciones en motor

--- PrimerProgreso/src/ejermotor/interfaz/Main.java
+++ PrimerProgreso/src/ejermotor/interfaz/Main.java
@@ -3,16 +3,28 @@ package ejermotor.interfaz;
 
 import ejermotor.negocio.Motor;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         Motor motorElectrico = new Motor();
-        motorElectrico.mostrarEstado();
-        motorElectrico.encender();
-        motorElectrico.encender();
-        motorElectrico.mostrarEstado();
-        motorElectrico.ajustarVelocidad(50);
-        motorElectrico.mostrarEstado();
-        motorElectrico.ajustarVelocidad(150);
+        try{
+            motorElectrico.mostrarEstado();
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,e.getMessage());
+        }
+
+        try{
+            motorElectrico.encender();
+            motorElectrico.encender();
+            motorElectrico.mostrarEstado();
+            motorElectrico.ajustarVelocidad(50);
+            motorElectrico.mostrarEstado();
+            motorElectrico.ajustarVelocidad(150);
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,e.getMessage());
+        }
+
 
     }
 }
--- PrimerProgreso/src/ejermotor/negocio/Motor.java
+++ PrimerProgreso/src/ejermotor/negocio/Motor.java
@@ -6,32 +6,34 @@ public class Motor {
 
     // encender(), apagar(), ajustarVelocidad(int nuevaVelocidad) y
     //mostrarEstado().
-    public void encender(){
+    public void encender() throws Exception{
         if(estado){
-            System.out.println("Motor ya está encendido.");
+            throw new Exception("Motor ya está encendido.");
+
         }else{
             estado = true;
         }
     }
-    public void apagado(){
+    public void apagado() throws Exception{
         if(estado){
             estado = false;
         }else{
-            System.out.println("Motor ya está apagado.");
+            throw new Exception("Motor ya está apagado.");
+
         }
     }
-    public void ajustarVelocidad(int nuevaVelocidad){
+    public void ajustarVelocidad(int nuevaVelocidad) throws Exception{
         if(nuevaVelocidad>0 && nuevaVelocidad<=100){
             velocidad = nuevaVelocidad;
-        }else{
-            System.out.println("La velocidad debe estar entre 1 y 100.");
-        }
+        }else
+            throw new Exception("La velocidad debe estar entre 1 y 100.");
+
     }
-    public void mostrarEstado(){
+    public void mostrarEstado() throws Exception{
         if(estado){
-            System.out.println("El motor está encendido.\nLa velocidad es: "+velocidad);
+            throw new Exception("El motor está encendido.\nLa velocidad es: "+velocidad);
         }else {
-            System.out.println("El motor está apagado.");
+            throw new Exception("El motor está apagado.");
         }
     }
 
